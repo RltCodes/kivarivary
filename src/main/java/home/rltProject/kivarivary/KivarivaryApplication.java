4614de9221b6eb111c0fade7ef4d5a5da776ef8b
RltCodes <lalahtahiana@gmail.com>
2024-06-11 22:18:44 +0300

    first commit

---
+++ src/main/java/home/rltProject/kivarivary/KivarivaryApplication.java
@@ -0,0 +1,13 @@
+package home.rltProject.kivarivary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KivarivaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KivarivaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/rltProject/kivarivary/KivarivaryApplicationTests.java
@@ -0,0 +1,13 @@
+package home.rltProject.kivarivary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KivarivaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
